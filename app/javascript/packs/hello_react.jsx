c31eac03abd67e5a8728caa103a61ba4fce24029
Joseph Hung <arixxle@gmail.com>
2020-05-07 11:59:47 +0800

    add scaffold and test react

--- app/javascript/packs/hello_react.jsx
+++ app/javascript/packs/hello_react.jsx
@@ -7,7 +7,14 @@ import ReactDOM from 'react-dom'
 import PropTypes from 'prop-types'
 
 const Hello = props => (
-  <div>Hello {props.name}!</div>
+  <div>
+    <h1>Hello {props.name}!</h1>
+    <ul>
+      <li>apple</li>
+      <li>banana</li>
+      <li>cherry</li>
+    </ul>  
+  </div>
 )
 
 Hello.defaultProps = {
